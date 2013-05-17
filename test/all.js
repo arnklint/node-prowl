d513cc91724023a30a8cd608d5439add798d64ee
Jonas Arnklint <jonas.arnklint@gmail.com>
2013-05-17 13:17:53 +0200

    catch errors, both http and prowl specific errors

--- test/all.js
+++ test/all.js
@@ -1,22 +1,31 @@
-var Prowl = require('../lib/prowl'),
-	p = new Prowl( process.env.PROWL_KEY ); // be sure to insert your own here
+var should = require('should');
+var Prowl = require('../lib/prowl');
+var p = new Prowl( process.env.PROWL_KEY ); // be sure to insert your own here
+var p2 = new Prowl( '' ); // be sure to insert your own here
 
 describe('Prowl Node.js API', function(){
 	it('pushes when minimum valid information is added', function(done){
 		p.push( 'i be in you iphonez', 'mah application', function( err, remaining ){
 			remaining.should.be.a('number');	
-      console.log( err, typeof err, remaining );
-			(err===null).should.eql(true);
+			should.not.exist(err);
+      done();	
 		});
-		done();	
 	});
 
   it('detects errors properly when pushing', function( done ){
-    p.push('tezt your iphonez', 'app', function( error, remaining ){
-      remaining.should.be.a('number');
-      error.should.be.a('object');
+    p2.push('tezt your iphonez', 'app', function( error, remaining ){
+      should.not.exist(remaining);
       error.message.should.be.a('string');
       error.code.should.be.a('string');
+      done();
+    });
+  });
+
+  it('callbacks with an error when request returns one', function( done ){
+    p2.push('hey', 'app', function( err, remaining ){
+      should.not.exist(remaining);
+      err.should.be.a("object");
+
       done();
     });
   });
