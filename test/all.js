1f93c779e0f27ec108169a87ada9ca93a462d6de
dxdc <dan@element26.net>
2020-01-17 19:28:10 -0600

    Modernize should calls

--- test/all.js
+++ test/all.js
@@ -6,7 +6,7 @@ var p2 = new Prowl( '' ); // be sure to insert your own here
 describe('Prowl Node.js API', function(){
 	it('pushes when minimum valid information is added', function(done){
 		p.push( 'i be in you iphonez', 'mah application', function( err, remaining ){
-			remaining.should.be.a('number');	
+			should(remaining).be.an.instanceOf(Number);
 			should.not.exist(err);
       done();	
 		});
@@ -15,8 +15,8 @@ describe('Prowl Node.js API', function(){
   it('detects errors properly when pushing', function( done ){
     p2.push('tezt your iphonez', 'app', function( error, remaining ){
       should.not.exist(remaining);
-      error.message.should.be.a('string');
-      error.code.should.be.a('string');
+      error.message.should.be.an.instanceOf(String);
+      error.code.should.be.an.instanceOf(String);
       done();
     });
   });
@@ -24,8 +24,7 @@ describe('Prowl Node.js API', function(){
   it('callbacks with an error when request returns one', function( done ){
     p2.push('hey', 'app', function( err, remaining ){
       should.not.exist(remaining);
-      err.should.be.a("object");
-
+      err.should.be.an.instanceOf(Object);
       done();
     });
   });
